57150266c1a98f8d5758f29754770118f1305551
Kishan Patel <kishan-patel@Kishans-Air.ht.home>
2024-08-04 11:35:48 -0400

    HideAPI

--- firebase.js
+++ firebase.js
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDbdckmaahRZciiMTN2rxExYUGg6I0h-Fw",
-  authDomain: "inventory-tracker-dd629.firebaseapp.com",
-  projectId: "inventory-tracker-dd629",
-  storageBucket: "inventory-tracker-dd629.appspot.com",
-  messagingSenderId: "108550297879",
-  appId: "1:108550297879:web:b2f8d6ee95b049c405ff52",
-  measurementId: "G-6ME51JLXTK"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
